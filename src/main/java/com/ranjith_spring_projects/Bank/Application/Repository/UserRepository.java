8bf7c8e22dd8227dc8d68570c2f4db9c2e3d1b2a
Ranjithkumar444 <ranjithrockzz289@gmail.com>
2025-02-02 16:26:24 +0530

    balance request endpoint added

--- src/main/java/com/ranjith_spring_projects/Bank/Application/Repository/UserRepository.java
+++ src/main/java/com/ranjith_spring_projects/Bank/Application/Repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.ranjith_spring_projects.Bank.Application.Repository;
 
+import com.ranjith_spring_projects.Bank.Application.Dto.BalanceRequest;
 import com.ranjith_spring_projects.Bank.Application.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByAccountNumber(String accountNumber);
 }
