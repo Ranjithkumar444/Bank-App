1dc6de0668a6937fbb83504c2e9ebfdcda27e372
Ranjithkumar444 <ranjithrockzz289@gmail.com>
2025-02-02 17:54:04 +0530

    the credit feature is added

--- src/main/java/com/ranjith_spring_projects/Bank/Application/Service/JWTService.java
+++ src/main/java/com/ranjith_spring_projects/Bank/Application/Service/JWTService.java
@@ -22,20 +22,11 @@ public class JWTService {
     // Secret key (ensure it is kept confidential)
     private static final String SECRET_KEY = "kJ9VbD8rA56yLqR7tPfXqkD3JnVsFwR23Ht7MzLsNpQ=";
 
-    /**
-     * Generate a JWT token for a given username.
-     *
-     * @param username the subject of the token
-     * @return a signed JWT token
-     */
     public String generateToken(String username) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, username);
     }
 
-    /**
-     * Create a token with claims, subject, issued date, expiration, and signature.
-     */
     private String createToken(Map<String, Object> claims, String subject) {
         Date now = new Date(System.currentTimeMillis());
         Date expiration = new Date(now.getTime() + 1000 * 60 * 60 * 10); // 10 hours expiration time
@@ -49,24 +40,15 @@ public class JWTService {
                 .compact();
     }
 
-    /**
-     * Extract the username from a JWT token.
-     */
     public String extractUserName(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    /**
-     * Extract a specific claim from the token.
-     */
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    /**
-     * Extract all claims from the token.
-     */
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getKey())
@@ -75,32 +57,19 @@ public class JWTService {
                 .getBody();
     }
 
-
-    /**
-     * Check if a token is valid by comparing the username and ensuring it's not expired.
-     */
     public boolean validateToken(String token, UserDetails userDetails) {
         final String userName = extractUserName(token);
         return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
-    /**
-     * Check if a token has expired.
-     */
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    /**
-     * Extract the expiration date from a token.
-     */
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    /**
-     * Decode the secret key from Base64 and return as SecretKey.
-     */
     private SecretKey getKey() {
         byte[] keyBytes = Base64.getDecoder().decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
